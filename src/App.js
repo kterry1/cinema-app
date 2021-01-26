6ffe0798d32f13c2991cc2dc987d7ab23c1edae0
Kevin <terrykevin1@gmail.com>
2021-01-26 13:48:53 -0500

    Ran Prettier to fix circleci check

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import React from 'react';
 const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Testing
         </a>
       </header>
