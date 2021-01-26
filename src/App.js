91da0f23cbefb4f90911940c329696a878d66bee
Kevin <terrykevin1@gmail.com>
2021-01-25 23:14:55 -0500

    Fixed prettier json file name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import React from "react";
+import './App.css';
+import React from 'react';
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Testing
         </a>
       </header>
